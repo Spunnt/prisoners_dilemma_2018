946f14da3bc75a5f1fd333b3ae65f2059d7daacf
Spunnt <46978501+Spunnt@users.noreply.github.com>
2019-01-25 21:29:07 -1000

    Update team2.py

--- 1_3_9-10_sourceFiles/team2.py
+++ 1_3_9-10_sourceFiles/team2.py
@@ -29,6 +29,8 @@ def move(my_history, their_history, my_score, their_score):
     #        return 'c'
 
     b_counter = their_history.count ('b')
+    if their_history [-3:-1]== 'ccc':
+        return 'b'
     if len(their_history)<=3: 
         return 'c'
     elif their_history[-1] == 'c' and their_history[-2] == 'c' and their_history[-3] == 'c':
